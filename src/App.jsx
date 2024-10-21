6e5763697c692dd12b0c6c637a055f2c5a1e094e
OMI786I <sohrawardy1998@gmail.com>
2024-10-21 15:14:01 +0600

    setup background for quiz game

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
-
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import TsParticleComponents from "./Components/TsParticleComponents";
 function App() {
-  return <></>;
+  return (
+    <>
+      <TsParticleComponents />
+    </>
+  );
 }
 
 export default App;
